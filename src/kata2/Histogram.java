436180a16bb4027ab67ff396e28f0d0bc9aa7d96
usuario <usuario@10.22.148.63>
2017-10-06 10:49:53 +0100

    Kata2; version4

--- src/kata2/Histogram.java
+++ src/kata2/Histogram.java
@@ -3,17 +3,17 @@ package kata2;
 import java.util.HashMap;
 import java.util.Map;
 
-public class Histogram {
+public class Histogram<T> {
     
-    private final int[] data;
+    private final T[] data;
     
-    public Histogram(int[] data){
+    public Histogram(T[] data){
         this.data = data;
     }
     
     public Map getHistogram(){
-        Map<Integer,Integer> histogram = new HashMap<>();
-        for (int value : data){
+        Map<T,Integer> histogram = new HashMap<>();
+        for (T value : data){
             histogram.put(value, histogram.containsKey(value) ? histogram.get(value) +1:1);
         }
         return histogram;
